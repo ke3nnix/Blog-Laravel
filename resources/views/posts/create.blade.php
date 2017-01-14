2c82a78870a72cbf2551695fab794e687a0e0afe
Kenny Horna <kennyhorna@gmail.com>
2017-01-14 16:52:46 -0500

    Add files via upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('titulo' , 'Crear nueva entrada')
 
+@section('stylesheets')
+
+	{!! Html::style('css/parsley.css') !!}
+
+@endsection
+
 @section('contenido')
 
 	<div class="row">
@@ -9,13 +15,13 @@
 			<h1>Crear un nueva entrada</h1>
 			<hr>
 
-			{!! Form::open(['route' => 'posts.store']) !!}
+			{!! Form::open(['route' => 'posts.store' , 'data-parsley-validate' => '']) !!}
 			    
 				{{ Form::label('titulo', 'Título:') }}
-				{{ Form::text('titulo' , null, array('class'=> 'form-control', 'style' => 'margin-bottom: 20px;')) }}
+				{{ Form::text('titulo' , null, array('class'=> 'form-control','required' => '', 'maxlength' => '255')) }}
 
-				{{ Form::label('cuerpo', 'Texto de la entrada:')}}
-				{{ Form::textarea('cuerpo', null, array('class' => 'form-control'))}}
+				{{ Form::label('cuerpo', 'Texto de la entrada:', array('style' => 'margin-top: 20px;'))}}
+				{{ Form::textarea('cuerpo', null, array('class' => 'form-control', 'required' => ''))}}
 
 
 				{{ Form::submit('Crear entrada', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
@@ -25,5 +31,11 @@
 
 		</div>
 	</div>
+
+@endsection
+
+@section('scripts')
+
+	{!! Html::script('js/parsley.min.js') !!}
 
 @endsection
