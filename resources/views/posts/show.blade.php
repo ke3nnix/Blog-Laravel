6a446b28d42bfd1ee824b410db597dc264b8326c
ke3nnix <kennyhorna@gmail.com>
2017-01-15 17:40:22 -0500

    ACT modificando interfaz show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,46 @@
 
 @section('contenido')
 
-	<h1>{{ $post->titulo }}</h1>
+	<div class="row">
+		<div class="col-md-8">
 
-	<hr>
+			<h1>{{ $post->titulo }}</h1>
 
-	<p class="lead">{{ $post->cuerpo }}</p>
+			<hr>
+
+			<p class="lead">{{ $post->cuerpo }}</p>
+
+		</div>
+
+		<div class="col-md-4">
+		
+			<div class="well">
+				
+				<dl class="dl-horizontal">
+					<dt>Creado: </dt>
+					<dd>{{ date("M j, Y g:i a", strtotime($post->created_at)) }}</dd>
+				</dl>
+
+				<dl class="dl-horizontal">
+					<dt>Actualizado: </dt>
+					<dd>{{ date("M j, Y g:i a", strtotime($post->updated_at)) }}</dd>
+				</dl>
+				<hr>
+				<div class="row">
+					<div class="col-sm-6">
+						{!! Html::linkRoute('posts.edit' , 'editar' , array($post->id) , array('class' => 'btn btn-primary btn-block')) !!}
+					</div>
+					<div class="col-sm-6">
+						{!! Html::linkRoute('posts.destroy' , 'eliminar' , array($post->id) , array('class' => 'btn btn-danger btn-block')) !!}
+					</div>
+					</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	
 
 @endsection
