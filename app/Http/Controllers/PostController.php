63dcb94081339ddc9b802b690769666971009acf
ke3nnix <kennyhorna@gmail.com>
2017-01-15 20:54:36 -0500

    ADD | EDIT() - EDIT view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -78,7 +78,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->withPost($post);
     }
 
     /**
